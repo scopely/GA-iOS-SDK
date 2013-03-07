6c08c57a8882727212f05537968d29851fa5c50c
Aleksandras S <aleksandras.smirnovas@gmail.com>
2013-03-07 10:15:09 +0200

    new binary version

---
+++ Demo/GA iOS Wrapper Demo/BatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  BatchViewController.h
+//  GA iOS Wrapper Demo
+//
+//  Created by Aleksandras Smirnovas on 3/6/13.
+//  Copyright (c) 2013 Aleksandras Smirnovas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BatchViewController : UIViewController
+
+@end
---
+++ Demo/GA iOS Wrapper Demo/BatchViewController.m
@@ -0,0 +1,38 @@
+//
+//  BatchViewController.m
+//  GA iOS Wrapper Demo
+//
+//  Created by Aleksandras Smirnovas on 3/6/13.
+//  Copyright (c) 2013 Aleksandras Smirnovas. All rights reserved.
+//
+
+#import "BatchViewController.h"
+
+@interface BatchViewController ()
+
+@end
+
+@implementation BatchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
