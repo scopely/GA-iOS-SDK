bfd682133e9e241843ae38e5a2de4fb72e04a51d
Aleksandras S <aleksandras.smirnovas@gmail.com>
2013-11-24 22:37:39 +0200

    CFRelease(cfstring); added

---
+++ Demo/CocoaPods/GA iOS SDK Demo/GA iOS SDK Demo/GABatchViewController.m
@@ -0,0 +1,38 @@
+//
+//  GABatchViewController.m
+//  GA iOS SDK Demo
+//
+//  Created by Aleksandras Smirnovas on 23/11/13.
+//  Copyright (c) 2013 coder.lt. All rights reserved.
+//
+
+#import "GABatchViewController.h"
+
+@interface GABatchViewController ()
+
+@end
+
+@implementation GABatchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Demo/CocoaPods/GA iOS SDK Demo/GA iOS SDK Demo/GAViewController.m
@@ -0,0 +1,29 @@
+//
+//  GAViewController.m
+//  GA iOS SDK Demo
+//
+//  Created by Aleksandras Smirnovas on 23/11/13.
+//  Copyright (c) 2013 coder.lt. All rights reserved.
+//
+
+#import "GAViewController.h"
+
+@interface GAViewController ()
+
+@end
+
+@implementation GAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
