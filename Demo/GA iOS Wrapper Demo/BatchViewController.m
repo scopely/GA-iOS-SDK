0b499dca6ab368d83411d669d5d8d6023241e2a6
Aleksandras S <aleksandras.smirnovas@gmail.com>
2013-11-28 20:27:44 +0200

    New methods calls added

--- Demo/GA iOS Wrapper Demo/BatchViewController.m
+++ Demo/GA iOS Wrapper Demo/BatchViewController.m
@@ -45,27 +45,51 @@
 
 -(IBAction)logUserData:(id)sender
 {
-    [GameAnalytics logUserDataWithParams:@{@"gender" : @"M", @"birth_year" : @1981, @"country" : @"LT", @"state" : @"VNO", @"friend_count" : @10}];
+    [GameAnalytics logUserDataWithGender:@"M"
+                               birthYear:@1981
+                             friendCount:@10];
 }
 
 -(IBAction)logBusinessData:(id)sender
 {
     [GameAnalytics logBusinessDataEvent:@"PurchaseWeapon:Shotgun"
                          currencyString:@"LTL"
-                           amountNumber:@1000
-                             withParams:@{@"area" : @"Level 1", @"x" : @1.0f, @"y" : @1.0f, @"z" : @1.0f}];
+                           amountNumber:@100
+                                   area:@"Level1"
+                                      x:@1.0f
+                                      y:@1.5f
+                                      z:@2.5f];
+    
+    //    [GameAnalytics logBusinessDataEvent:@"PurchaseWeapon:Shotgun"
+    //                         currencyString:@"LTL"
+    //                           amountNumber:@1000
+    //                             withParams:@{@"area" : @"Level 1", @"x" : @1.0f, @"y" : @1.0f, @"z" : @1.0f}];
 }
 
 -(IBAction)logGameDesingData:(id)sender
 {
     [GameAnalytics logGameDesignDataEvent:@"PickedUpAmmo:Shotgun"
-                               withParams:@{@"area" : @"Level 1", @"x" : @1.0f, @"y" : @1.0f, @"z" : @1.0f, @"value" : @1.0f}];
+                                    value:@1.0f
+                                     area:@"Level 1"
+                                        x:@1.0f
+                                        y:@2.0f
+                                        z:@3.0f];
+    
+    //    [GameAnalytics logGameDesignDataEvent:@"PickedUpAmmo:Shotgun"
+    //                               withParams:@{@"area" : @"Level 1", @"x" : @1.0f, @"y" : @1.0f, @"z" : @1.0f, @"value" : @1.0f}];
 }
 
 -(IBAction)logQAData:(id)sender
 {
     [GameAnalytics logQualityAssuranceDataEvent:@"Exceptaion:NullReferenceException"
-                                     withParams:@{ @"area" : @"Level 1", @"x" : @1.0f, @"y" : @1.0f, @"z" : @1.0f, @"message" : @"at Infragistics.Windows.Internal.TileManager.ItemRowColumnSizeInfo.."}];
+                                        message:@"at Infragistics.Windows.Internal.TileManager.ItemRowColumnSizeInfo.."
+                                           area:@"area"
+                                              x:@1
+                                              y:@2
+                                              z:@3];
+    
+    //    [GameAnalytics logQualityAssuranceDataEvent:@"Exceptaion:NullReferenceException"
+    //                                     withParams:@{ @"area" : @"Level 1", @"x" : @1.0f, @"y" : @1.0f, @"z" : @1.0f, @"message" : @"at Infragistics.Windows.Internal.TileManager.ItemRowColumnSizeInfo.."}];
 }
 
 -(IBAction)toggleDebugLog:(id)sender
